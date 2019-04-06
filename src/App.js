c2f522e583cf8a3b8ea714c477cf902abf961b8b
JoanLazo <joan162581@gmail.com>
2019-04-06 17:43:27 -0500

    cambiando la estructura de mis componentes a funciones instalando slint y agregando libreria para router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-one-expression-per-line */
+// eslint-disable-next-line  reaccion / jsx-filename-extension
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 // import Inicio from './inicio/inicio'
 import Menulist from './menu/menu-list';
@@ -6,89 +13,95 @@ import Menulist from './menu/menu-list';
 
 // const db = firebase.firestore();
 
+function App() {
+  const [data, setData] = useState({ menu: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(
+          'https://joanlazo.github.io/LIM008-fe-burger-queen/src/data-menu/menu.json',
+        );
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      menu: []
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
-  }
 
-  componentDidMount() {
-    fetch("https://joanlazo.github.io/LIM008-fe-burger-queen/src/data-menu/menu.json")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            menu: result.menu
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+    fetchData();
+  }, []);
 
-  render() {
-    const { error, isLoaded, menu } = this.state;
-    if (error) {
-     return <div>Error: {error.message}</div>;
-   } else if (!isLoaded) {
-     return <div>Loading...</div>;
-   } else {
-    return (
-      <div className="container-all">
+  return (
+    <div className="container-all">
         <div className="burger-header color-one">
-           <h1 className="ml-3 mt-3">BURGER QUEEN</h1>
+          <h1 className="ml-3 mt-3">BURGER QUEEN</h1>
         </div>
         <div className="d-flex flex-row justify-content-around py-3 color-height">
-        <button className="btn-menu color-height color-white"><a className="links color-white"
-        href="#desayuno" 
-         rel="noopener noreferrer">DESAYUNO</a></button>
-        <button className="btn-menu color-height color-white"><a className="links color-white"
-        href="#resto" 
-         rel="noopener noreferrer">RESTO DEL DÍA</a></button>
+          <button className="btn-menu color-height color-white" type="button">
+            <a
+              className="links color-white"
+              href="#desayuno"
+              rel="noopener noreferrer"
+            // eslint-disable-next-line react/jsx-one-expression-per-line
+            >DESAYUNO
+            </a>
+          </button>
+          <button className="btn-menu color-height color-white" type="button">
+            <a
+              className="links color-white"
+              href="#resto"
+              rel="noopener noreferrer"
+            >RESTO DEL DÍA
+            </a>
+          </button>
         </div>
 
         <div className="burger-body">
-      
-        <div className="container">
-          <div className="row"> 
-           <div className="col-5">
-             <div className="d-flex flex-column">
-          {menu.map(elem => (
-            <button key={elem.id} className="btn-menu color-four my-2">{elem.item}  s/.{elem.precio}</button>
-          ))}
-             </div>
-            </div>
 
-            <div className="col-7">
-            <div>
-            <Menulist/>
+          <div className="container">
+            <div className="row">
+              <div className="col-5">
+                <div>
+
+                {isError && <div>Something went wrong ...</div>}
+
+                {isLoading ? (
+                   <div>Loading ...</div>
+                ) :  (
+                  <div className="d-flex flex-column">
+                  {data.menu.map(elem => (
+                    <button key={elem.id} className="btn-menu color-four my-2" type="button">
+                      {elem.item}
+                      {' '}
+                      s/.
+                      {elem.precio}
+                    </button>
+                  ))}
+                 </div>
+                )};
+                </div>
+              </div>
+
+              <div className="col-7">
+                <div>
+                  <Menulist />
+                </div>
+              </div>
+
             </div>
           </div>
 
-           </div>
-          </div>
 
-          
-         </div>
+        </div>
 
       </div>
-    );
-  }
-}
+  );
 }
 
 export default App;
--- src/menu/menu-list.jsx
+++ src/menu/menu-list.jsx
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable padded-blocks */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-closing-tag-location */
+import React, { useState } from 'react';
 import './menu.css';
 // import firebase from './config/config.js'
 // const db = firebase.firestore();
 
-class Menulist extends Component {
-  
-    render() {
-      return (
-       
-          <div className="my-4 color-white-b">
-            <form className="py-3">
-                <div className="d-flex justify-content-between align-items-center">
-                  <h3>Producto</h3>
-                  <h3>Precio</h3>
-                  <h3>Cantidad</h3>
-                </div>
-                <span></span>
-                <span></span>
-                <img src='https://user-images.githubusercontent.com/44485081/55659563-e0045700-57c7-11e9-971e-d57e2b55ddf7.png' className="delete-img" alt="logo" />
-            </form>
+function Menulist() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+      <div className="my-4 color-white-b">
+        <div className="py-3">
+          <div className="d-flex justify-content-between align-items-center">
+              <h3>Producto</h3>
+              <h3>Precio</h3>
+              <h3>Cantidad</h3>
+            </div>
+          <span />
+          <span />
+      <button className="btn-ico" onClick={() => setCount(count - 1)} type="button"><img src="https://user-images.githubusercontent.com/44485081/55675961-40f76200-5890-11e9-97c8-71271a02e4db.png" className="ico-menu-list" alt="menos" /></button>
+      Cantidad :{' '}{count}
+      <button className="btn-ico" onClick={() => setCount(count + 1)} type="button"><img src="https://user-images.githubusercontent.com/44485081/55675953-24f3c080-5890-11e9-8aae-0b9051a2051f.png" className="ico-menu-list" alt="más" /></button>
+      <button className="btn-ico" type="button"><img src="https://user-images.githubusercontent.com/44485081/55659563-e0045700-57c7-11e9-971e-d57e2b55ddf7.png" className="ico-menu-list" alt="logo" /></button>
         </div>
-      );
-    }
-  }
-  
-  export default Menulist;
+      </div>
+  );
+}
+
+
+export default Menulist;
