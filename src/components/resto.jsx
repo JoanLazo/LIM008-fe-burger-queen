1aab8819cd3347a804c3786c5e80686339564731
JoanLazo <joan162581@gmail.com>
2019-04-08 15:51:02 -0500

    creando la base de datos pintando para resto del dia

--- src/components/resto.jsx
+++ src/components/resto.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable linebreak-style */
 /* eslint-disable padded-blocks */
 /* eslint-disable react/jsx-indent-props */
@@ -5,15 +6,61 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Menulist from '../menu/menu-list';
 
-const Resto = () => (
+const Resto = () => {
+  const [data, setData] = useState({ menu: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(
+          'https://joanlazo.github.io/LIM008-fe-burger-queen/src/data-menu/menu.json',
+        );
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
 <div className="container">
     <div className="row">
       <div className="col-5">
       <h1>Resto del día</h1>
+      <div>
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+   <div>Loading ...</div>
+      ) : (
+      <div className="d-flex flex-column flex-nowrap">
+        {data.menu.map((elem) => {
+          if (elem.categoría === 'resto del día') {
+            return (
+              <button key={elem.id} className="btn-menu color-four my-2" type="button">
+                {elem.item}
+                {' '}
+                s/.
+                {elem.precio}
+             </button>
+            );
+          }
+        })}
+        </div>
+      )}
+    </div>
       <div className="col-7">
                 <div>
                   <Menulist />
@@ -22,5 +69,6 @@ const Resto = () => (
               </div>
         </div>
         </div>
-);
+  );
+};
 export default Resto;
