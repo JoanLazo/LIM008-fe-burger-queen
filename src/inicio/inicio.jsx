100f98e96b185e0c16ca08e98e0744acd04c5e21
JoanLazo <joan162581@gmail.com>
2019-04-08 19:26:28 -0500

    agregando cambios

--- src/inicio/inicio.jsx
+++ src/inicio/inicio.jsx
@@ -1,31 +1,33 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './inicio.css';
 
 const Inicio = () => (
-  <div className="burger-fondo">
-    <div className="oscuro">
-      <h1 className="py-5 text-center text-title">
+  <Router>
+    <div className="burger-fondo">
+      <div className="oscuro">
+        <h1 className="py-5 text-center text-title">
            BURGER QUEEN
-      </h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="d-flex flex-row  justify-content-center align-items-center burger-header-inicio">
-              <button type="button" className="ml-5 mr-5 btn-menu-inicio color-six links">
-                <Link className="links" to="/mesero">MESERO</Link>
-              </button>
-              <button type="button" className="ml-5 mr-5 btn-menu-inicio color-five">
-                <Link className="links" to="/cocinero">COCINERO</Link>
-              </button>
+        </h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="d-flex flex-row  justify-content-center align-items-center burger-header-inicio">
+                <button type="button" className="ml-5 mr-5 btn-menu-inicio color-six links">
+                  <Link className="links" to="/mesero">MESERO</Link>
+                </button>
+                <button type="button" className="ml-5 mr-5 btn-menu-inicio color-five">
+                  <Link className="links" to="/cocinero">COCINERO</Link>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </Router>
 );
 
 export default Inicio;
